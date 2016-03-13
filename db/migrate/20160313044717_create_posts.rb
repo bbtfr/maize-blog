c5de009197664ca4073af1932c3a8a31b5a20a7a
Theo <bbtfrr@gmail.com>
2016-03-13 20:00:31 +0800

    Lession 3

---
+++ db/migrate/20160313044717_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :author
+      t.datetime :published_at
+
+      t.timestamps null: false
+    end
+  end
+end
